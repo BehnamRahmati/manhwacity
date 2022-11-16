17da708148d206e3903511acae2f0ca73d0b5611
Behnam Rahmati <49502003+BehnamRahmati@users.noreply.github.com>
2022-11-16 21:55:37 +0000

    initial commit

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,8 @@
+
+export interface IFooter {}
+
+const Footer: React.FC<IFooter> = () => {
+	return <div>footer</div>;
+};
+
+export default Footer;
---
+++ components/header/Header.tsx
@@ -0,0 +1,8 @@
+
+export interface IHeader {}
+
+const Header: React.FC<IHeader> = () => {
+	return <div>header</div>;
+};
+
+export default Header;
